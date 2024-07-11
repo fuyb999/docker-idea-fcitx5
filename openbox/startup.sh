1263f6e3b2d0a9977f2a4e9e36696a0d85964f15
fuyb <656452024@qq.com>
2024-07-11 08:52:33 +0800

    update

--- openbox/startup.sh
+++ openbox/startup.sh
@@ -1,4 +1,16 @@
 touch /var/run/openbox/openbox.ready &
 
-# 启动时候禁用unicode与简繁切换以屏蔽ctrl+shift+u,ctrl+shift+f
-fcitx5 --disable=unicode,chttrans &
+# 禁用unicode,chttrans(简繁切换)插件，以解决ctrl+shift+f,ctrl+shift+u快捷键冲突
+fcitx5 --disable=dbus,wayland,unicode,chttrans &
+
+# dbus
+#ENV DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/0/bus"
+#RUN add-pkg dbus dbus-x11 \
+#   &&  mkdir -p /var/run/dbus \
+#   &&  chmod 755 /var/run/dbus
+#addgroup messagebus
+#useradd -g messagebus messagebus
+#chown -R root:root /tmp/run/user/app
+#dbus-launch --sh-syntax --exit-with-session > /dev/null &
+#dbus-daemon --system &
+#dbus-launch fcitx5 --disable=wayland,unicode,chttrans &
