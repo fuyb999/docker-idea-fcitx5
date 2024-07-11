e0cb0043abda339ec4c7eac3851011b189bf9523
fuyb <656452024@qq.com>
2024-07-11 18:17:25 +0800

    add jdk conda node

---
+++ rootfs/etc/cont-init.d/57-install-jdk.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -e # Exit immediately if a command exits with a non-zero status.
+set -u # Treat unset variables as an error.
+
+log() {
+    echo "[cont-init.d] $(basename $0): $*"
+}
+
+export PKG_PATH=/config/packages
+
+if [ ! -d "$PKG_PATH" ]; then
+  mkdir -p ${PKG_PATH}
+fi
+
+export JAVA_HOME=$XDG_CONFIG_HOME/jdk-${JDK_VERSION}
+export PATH=$JAVA_HOME/bin;$PATH
+
+if [ -d "${JAVA_HOME}" ]; then
+  exit 0
+fi
+
+if [ ! -f "${PKG_PATH}/jdk-${JDK_VERSION}_linux-x64_bin.tar.gz" ]; then
+  wget https://download.oracle.com/java/17/archive/jdk-${JDK_VERSION}_linux-x64_bin.tar.gz -O ${PKG_PATH}/jdk-${JDK_VERSION}_linux-x64_bin.tar.gz
+fi
+
+tar -xzf ${PKG_PATH}/jdk-${JDK_VERSION}_linux-x64_bin.tar.gz -C $XDG_CONFIG_HOME
---
+++ rootfs/etc/cont-init.d/60-install-anaconda3.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -e # Exit immediately if a command exits with a non-zero status.
+set -u # Treat unset variables as an error.
+
+log() {
+    echo "[cont-init.d] $(basename $0): $*"
+}
+
+#apt-get install -y libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
+
+export ANACONDA_HOME=$XDG_CONFIG_HOME/anaconda3
+
+which which conda > /dev/null 2>&1
+if [ "$?" -eq "0" ]; then
+  exit 0
+fi
+
+tee $XDG_CONFIG_HOME/.condarc << EOF
+envs_dirs:
+  - ${XDG_CONFIG_HOME}/Anaconda3/envs
+  - ${XDG_CONFIG_HOME}/.conda/envs
+  - ${XDG_CONFIG_HOME}/conda/conda/envs
+pkgs_dirs:
+  - ${XDG_CONFIG_HOME}/Anaconda3/pkgs
+  - ${XDG_CONFIG_HOME}/.conda/pkgs
+  - ${XDG_CONFIG_HOME}/conda/conda/pkgs
+auto_activate_base: true
+show_channel_urls: true
+channels:
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main/
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
+  - defaults
+EOF
+
+chmod 777 $XDG_CONFIG_HOME/.condarc
+
+if [ ! -f "${PKG_PATH}/Anaconda3-${CONDA_VERSION}-Linux-x86_64.sh" ]; then
+  wget -nc https://repo.anaconda.com/archive/Anaconda3-${CONDA_VERSION}-Linux-x86_64.sh -O ${PKG_PATH}/Anaconda3-${CONDA_VERSION}-Linux-x86_64.sh
+fi
+
+bash ${PKG_PATH}/Anaconda3-${CONDA_VERSION}-Linux-x86_64.sh -b -p $ANACONDA_HOME -f
+
+chmod -R 777 $ANACONDA_HOME && $ANACONDA_HOME/bin/conda init bash
+source /config/.bashrc
+
+#conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
+#conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main/
+#conda config --set show_channel_urls yes
+#conda config --set auto_activate_base true
