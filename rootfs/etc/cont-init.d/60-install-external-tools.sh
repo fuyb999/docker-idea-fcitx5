6d4f1c265563ecd197ce149d00b33b79939c0991
fuyb <fuyb@g.com>
2024-10-10 22:14:50 +0800

    update

---
+++ rootfs/etc/cont-init.d/60-install-external-tools.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -e # Exit immediately if a command exits with a non-zero status.
+set -u # Treat unset variables as an error.
+
+log() {
+    echo "[cont-init.d] $(basename $0): $*"
+}
